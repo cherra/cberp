8f92a585c4b1bfa4d4d6f5c9a1bf9f44dd30b0c5
cherra <jorge@matica.mx>
2013-10-01 11:47:41 -0500

    Modelo Concepto_comision

---
+++ application/models/catalogo/concepto_comision.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * Description of concepto_comision
+ *
+ * @author cherra
+ */
+class Concepto_comision extends CI_Model {
+    
+    private $tbl = 'ConceptoComision';
+    
+    /*
+     * Cuenta todos los registros utilizando un filtro de busqueda
+     */
+    function count_all( $filtro = NULL ) {
+        if(!empty($filtro)){
+            $filtro = explode(' ', $filtro);
+            foreach($filtro as $f){
+                $this->db->or_like('descripcion',$f);
+            }
+        }
+        $query = $this->db->get($this->tbl);
+        return $query->num_rows();
+    }
+    
+    /**
+     *  Obtiene todos los registros de la tabla
+     */
+    function get_all() {
+        $this->db->order_by('descripcion','asc');
+        return $this->db->get($this->tbl);
+    }
+    
+    /**
+    * Cantidad de registros por pagina
+    */
+    function get_paged_list($limit = NULL, $offset = 0, $filtro = NULL) {
+        if(!empty($filtro)){
+            $filtro = explode(' ', $filtro);
+            foreach($filtro as $f){
+                $this->db->or_like('descripcion',$f);
+            }
+        }
+        $this->db->order_by('descripcion','asc');
+        return $this->db->get($this->tbl, $limit, $offset);
+    }
+    
+    /**
+    * Obtener por id
+    */
+    function get_by_id($id) {
+        $this->db->where('id_concepto_comision', $id);
+        return $this->db->get($this->tbl);
+    }
+    
+    /**
+    * Alta
+    */
+    function save( $datos ) {
+        $this->db->insert($this->tbl, $datos);
+        return $this->db->insert_id();
+    }
+
+    /**
+    * Actualizar por id
+    */
+    function update($id, $datos) {
+        $this->db->where('id_concepto_comision', $id);
+        $this->db->update($this->tbl, $datos);
+    }
+
+    /**
+    * Eliminar por id
+    */
+    function delete($id) {
+        $this->db->where('id_concepto_comision', $id);
+        $this->db->delete($this->tbl);
+    } 
+}
+?>

88d05aa3b05098a364ae6e4631962910d1c82916
cherra <jorge@matica.mx>
2013-10-29 17:35:36 -0600

    CRUD de Lineas

--- application/controllers/catalogos/articulos.php
+++ application/controllers/catalogos/articulos.php
@@ -13,6 +13,127 @@ class Articulos extends CI_Controller {
         parent::__construct();
     }
     
+    public function lineas( $offset = 0 ){
+        $this->load->model('catalogos/linea','l');
+        
+        $this->config->load("pagination");
+    	
+        $data['titulo'] = 'Lineas <small>Lista</small>';
+    	$data['link_add'] = $this->folder.$this->clase.'lineas_agregar';
+    	$data['action'] = $this->folder.$this->clase.'lineas';
+        
+        // Filtro de busqueda (se almacenan en la sesión a través de un hook)
+        $filtro = $this->session->userdata('filtro');
+        if($filtro)
+            $data['filtro'] = $filtro;
+        
+        $page_limit = $this->config->item("per_page");
+    	$datos = $this->l->get_paged_list($page_limit, $offset, $filtro)->result();
+    	
+        // generar paginacion
+    	$this->load->library('pagination');
+    	$config['base_url'] = site_url($this->folder.$this->clase.'lineas');
+    	$config['total_rows'] = $this->l->count_all($filtro);
+    	$config['per_page'] = $page_limit;
+    	$config['uri_segment'] = 4;
+    	$this->pagination->initialize($config);
+    	$data['pagination'] = $this->pagination->create_links();
+    	
+    	// generar tabla
+    	$this->load->library('table');
+    	$this->table->set_empty('&nbsp;');
+    	$tmpl = array ( 'table_open' => '<table class="' . $this->config->item('tabla_css') . '" >' );
+    	$this->table->set_template($tmpl);
+    	$this->table->set_heading('Nombre', '');
+    	foreach ($datos as $d) {
+            $linea = $this->l->get_by_id($d->id_linea)->row();
+            $this->table->add_row(
+                    $d->nombre,
+                    anchor($this->folder.$this->clase.'lineas_ver/' . $d->id_linea, '<span class="glyphicon glyphicon-edit"></span>')
+            );
+    	}
+    	$data['table'] = $this->table->generate();
+    	
+    	$this->load->view('lista', $data);
+    }
+    
+    /*
+     * Agregar una linea
+     */
+    public function lineas_agregar() {
+        $this->load->model('catalogos/linea','l');
+        
+    	$data['titulo'] = 'Lineas <small>Registro nuevo</small>';
+    	$data['link_back'] = $this->folder.$this->clase.'lineas';
+    
+    	$data['action'] = $this->folder.$this->clase.'lineas_agregar';
+    	if ( ($datos = $this->input->post()) ) {
+    		if( ($id = $this->l->save($datos)) ){
+                    $this->session->set_flashdata('mensaje',$this->config->item('create_success'));
+                    redirect($this->folder.$this->clase.'lineas_ver/'.$id);
+                }else{
+                    $this->session->set_flashdata('mensaje',$this->config->item('error'));
+                    redirect($this->folder.$this->clase.'lineas_agregar');
+                }
+    		//$data['mensaje'] = '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>¡Registro exitoso!</div>';
+    	}
+        $this->load->view('catalogos/lineas/formulario', $data);
+    }
+    
+    
+    /*
+     * Vista previa de la linea
+     */
+    public function lineas_ver( $id = NULL ) {
+        $this->load->model('catalogos/linea','l');
+        
+        $linea = $this->l->get_by_id($id);
+        if ( empty($id) OR $linea->num_rows() <= 0) {
+            redirect($this->folder.$this->clase.'lineas');
+    	}
+    	
+    	$data['titulo'] = 'Lineas <small>Ver registro</small>';
+    	$data['link_back'] = $this->folder.$this->clase.'lineas';
+        if($this->session->flashdata('mensaje'))
+            $data['mensaje'] = $this->session->flashdata('mensaje');
+    	$data['action'] = $this->folder.$this->clase.'lineas_editar/' . $id;
+
+    	$data['datos'] = $this->l->get_by_id($id)->row();
+        
+        $this->load->view('catalogos/lineas/vista', $data);
+    }
+    
+    /*
+     * Editar una linea
+     */
+    public function lineas_editar( $id = NULL ) {
+        $this->load->model('catalogos/linea','l');
+        
+        $linea = $this->l->get_by_id($id);
+        if ( empty($id) OR $linea->num_rows() <= 0) {
+            redirect($this->folder.$this->clase.'lineas');
+    	}
+    	
+    	$data['titulo'] = 'Lineas <small>Editar registro</small>';
+    	$data['link_back'] = $this->folder.$this->clase.'lineas_ver/'.$id;
+    	$data['mensaje'] = '';
+    	$data['action'] = $this->folder.$this->clase.'lineas_editar/' . $id;
+    	 
+    	if ( ($datos = $this->input->post()) ) {
+    		$this->l->update($id, $datos);
+                $this->session->set_flashdata('mensaje',$this->config->item('update_success'));
+    		//$mensaje = '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>¡Registro modificado!</div>';
+                redirect($this->folder.$this->clase.'lineas_ver/'.$id);
+    	}
+
+    	$data['datos'] = $this->l->get_by_id($id)->row();
+        
+        $this->load->view('catalogos/lineas/formulario', $data);
+    }
+    
+    /*
+     * Listado de productos
+     */
     public function productos( $offset = 0 ){
         $this->load->model('catalogos/producto','p');
         $this->load->model('catalogos/linea','l');
@@ -45,7 +166,7 @@ class Articulos extends CI_Controller {
     	$this->table->set_empty('&nbsp;');
     	$tmpl = array ( 'table_open' => '<table class="' . $this->config->item('tabla_css') . '" >' );
     	$this->table->set_template($tmpl);
-    	$this->table->set_heading('Nombre', 'Linea');
+    	$this->table->set_heading('Nombre', 'Linea', '');
     	foreach ($datos as $d) {
             $linea = $this->l->get_by_id($d->id_linea)->row();
             $this->table->add_row(
@@ -90,8 +211,7 @@ class Articulos extends CI_Controller {
         
         $producto = $this->p->get_by_id($id);
         if ( empty($id) OR $producto->num_rows() <= 0) {
-            die($id);
-    		redirect($this->folder.$this->clase.'productos');
+            redirect($this->folder.$this->clase.'productos');
     	}
     	
     	$data['titulo'] = 'Productos <small>Ver registro</small>';
@@ -115,8 +235,7 @@ class Articulos extends CI_Controller {
         
         $producto = $this->p->get_by_id($id);
         if ( empty($id) OR $producto->num_rows() <= 0) {
-            die($id);
-    		redirect($this->folder.$this->clase.'productos');
+            redirect($this->folder.$this->clase.'productos');
     	}
     	
     	$data['titulo'] = 'Productos <small>Editar registro</small>';
